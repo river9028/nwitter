0522bdda1f514b3c57aecf57517f4e1505c09b95
river <river9028@gmail.com>
2020-09-26 16:24:33 +0900

    :zap: firebase storage 파일 삭제

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import AppRouter from 'components/Router';
 import { authService } from 'fbase';
-import { auth } from 'firebase';
 
 function App() {
   const [init, setInit] = useState(false);
--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -1,4 +1,4 @@
-import { dbService } from 'fbase';
+import { dbService, storageService } from 'fbase';
 import React, { useState } from 'react';
 
 function Nweet({ nweetObj, isOwner }) {
@@ -9,6 +9,7 @@ function Nweet({ nweetObj, isOwner }) {
     if (ok) {
       // delete nweet
       await dbService.doc(`nweets/${nweetObj.id}`).delete();
+      await storageService.refFromURL(nweetObj.attachmentURL).delete();
     }
   };
   const toggleEditing = () => setEditing((prev) => !prev);
