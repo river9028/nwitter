07e6e2ba6a156a6b780de956fc6a6740593c5008
river <river9028@gmail.com>
2020-09-26 17:20:28 +0900

    :zap: 프로필 이름 업데이트 구현

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,12 +1,27 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { authService, dbService } from 'fbase';
 import { useHistory } from 'react-router-dom';
 
 const Profile = ({ userObj }) => {
   const history = useHistory();
+  const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
   const onLogOutClick = () => {
     authService.signOut();
     history.push('/');
+  };
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewDisplayName(value);
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    if (userObj.displayName !== newDisplayName) {
+      await userObj.updateProfile({
+        displayName: newDisplayName,
+      });
+    }
   };
   const getMyNweet = async () => {
     const nweets = await dbService
@@ -17,11 +32,20 @@ const Profile = ({ userObj }) => {
     console.log(nweets.docs.map((doc) => doc.data()));
   };
 
-  useEffect(() => {
-    getMyNweet();
-  }, []);
+  // useEffect(() => {
+  //   getMyNweet();
+  // }, []);
   return (
     <>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          onChange={onChange}
+          placeholder="DisplayName"
+          value={newDisplayName}
+        />
+        <input type="submit" value="Update Profile" />
+      </form>
       <button onClick={onLogOutClick}>Log out</button>
     </>
   );
