ea352266d8049e69af33239f6b6a5852cb9b7b4a
river <river9028@gmail.com>
2020-09-28 14:23:33 +0900

    Updates

---
+++ precache-manifest.48ef0d453e547e57544c50dc0d66e09c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15a57f4c0b5d55a175843a0b695fff5e",
+    "url": "/nwitter/index.html"
+  },
+  {
+    "revision": "886a7d6634d63ae09c96",
+    "url": "/nwitter/static/css/main.51fba151.chunk.css"
+  },
+  {
+    "revision": "6a2d1eb8a3fa480b7c29",
+    "url": "/nwitter/static/js/2.b7ab1d37.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/nwitter/static/js/2.b7ab1d37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "886a7d6634d63ae09c96",
+    "url": "/nwitter/static/js/main.28e87018.chunk.js"
+  },
+  {
+    "revision": "97fb235846154051176c",
+    "url": "/nwitter/static/js/runtime-main.222ae764.js"
+  }
+]);
